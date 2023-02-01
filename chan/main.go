e09888cba2dfbb711294e7e3ac04ad75e52c4d18
蔡浩 <caihh@yoozoo.com>
2023-02-01 10:13:54 +0800

    chan test

---
+++ chan/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+func main() {
+	type ChanStruct struct {
+		StartTime int64
+		Index     int
+		Num       int
+	}
+	chanTest := make(chan *ChanStruct, 1000)
+	var M = 1000
+	var N = 100
+
+	for i := 0; i < M; i++ {
+		i := i
+		go func() {
+			for j := 0; j < N; j++ {
+				chanTest <- &ChanStruct{
+					Index:     i,
+					Num:       j,
+					StartTime: time.Now().UnixMilli(),
+				}
+			}
+		}()
+	}
+	time.Sleep(time.Second * 2)
+
+	var max int
+	var index, count int
+	m := make([]int, M)
+
+	var totalCount int
+	for {
+		select {
+		case data := <-chanTest:
+			m[data.Index]++
+			if m[data.Index] > max {
+				max = m[data.Index]
+			}
+			if data.Num == 0 {
+				totalCount++
+				if totalCount == M {
+					sort.Slice(m, func(i, j int) bool {
+						return m[i] > m[j]
+					})
+					fmt.Println(m)
+					return
+				}
+			}
+			time.Sleep(time.Microsecond * time.Duration(rand.Intn(100)+100))
+			count++
+			if count >= M*N {
+				fmt.Println(index, max)
+				return
+			}
+		}
+	}
+}
